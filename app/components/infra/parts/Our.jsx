4db3630fd2dbf2301d4180901372bc8b03f3b4fd
yogesh <yogesh.mobyink@gmail.com>
2024-10-22 15:51:58 +0530

    kjk

--- app/components/infra/parts/Our.jsx
+++ app/components/infra/parts/Our.jsx
@@ -6,10 +6,10 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import img1 from "./assets/main/1.JPG";
-import img2 from "./assets/main/2.JPG";
-import img3 from "./assets/main/3.JPG";
-import img4 from "./assets/main/4.JPG";
+import img1 from "./assets/Main/1.JPG";
+import img2 from "./assets/Main/2.JPG";
+import img3 from "./assets/Main/3.JPG";
+import img4 from "./assets/Main/4.JPG";
 
 export default function Our() {
   var settings = {
